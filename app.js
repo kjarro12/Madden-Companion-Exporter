7fde1577e542ed679562c2074cda80c56c779dcb
kjarro12 <48162725+kjarro12@users.noreply.github.com>
2019-08-05 20:25:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./cfm-spreadsheet-firebase-adminsdk-dfwuc-dce13e4ed4.json");
+const serviceAccount = require("./cfm-spreadsheet-firebase-adminsdk-dfwuc-1d06832542.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
